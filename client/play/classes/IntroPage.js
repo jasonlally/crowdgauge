1cc18711eee5e106c029c7088d341e83506de943
Jason Lally <jason.lally@gmail.com>
2013-07-24 08:16:05 -0600

    turn on survey

--- client/play/classes/IntroPage.js
+++ client/play/classes/IntroPage.js
@@ -109,7 +109,7 @@
         };
 
         var _initialize = function () {
-            /*
+
             _populateControls($('#introFrm'));
             $('.demoInput select').change(function () {
                 $('label[for="'+$(this).attr("id")+'"]').toggleClass("nonvalid", SAS.controlUtilsInstance.isPrompt($(this).val()));
@@ -117,7 +117,7 @@
             _$btnStart.click(function () {
                 if (_validate()) _onStartClick();
 //                _onStartClick();
-            });    */
+            });
         };
         //endregion
 
